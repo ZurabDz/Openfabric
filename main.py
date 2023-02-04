44f3993e1125c07dad35223a324e72f3fc791fb3
ZurabDz <zuradzindzibadze06@gmail.com>
2023-02-04 14:43:16 +0400

    small solution

--- main.py
+++ main.py
@@ -5,6 +5,12 @@ from ontology_dc8f06af066e4a7880a5938933236037.simple_text import SimpleText
 from openfabric_pysdk.context import OpenfabricExecutionRay
 from openfabric_pysdk.loader import ConfigClass
 from time import time
+from datasets import load_dataset
+from sentence_transformers import SentenceTransformer, util
+import torch
+from random import choice
+
+model = SentenceTransformer('all-MiniLM-L6-v2')
 
 
 ############################################################
@@ -15,14 +21,48 @@ def config(configuration: ConfigClass):
     pass
 
 
+def precompute_embeddings():
+    dataset = load_dataset("sciq")
+
+    possible_questions = [value['question'] for value in dataset['test']]
+
+    question_answer_mapper = {
+        value['question']: value['correct_answer'] for value in dataset['train']}
+    questions = list(question_answer_mapper.keys())
+    corpus_embeddings = model.encode(questions, convert_to_tensor=True)
+
+    return corpus_embeddings, questions, question_answer_mapper, possible_questions
+
+
+corpus_embeddings, questions, question_answer_mapper, possible_questions = precompute_embeddings()
+
+
+def find_match(query):
+    query_embedding = model.encode(query, convert_to_tensor=True)
+
+    cos_scores = util.cos_sim(corpus_embeddings, query_embedding)[0]
+    # For know choose top 1 result
+    top_results = torch.topk(cos_scores, k=1)
+    scores, indicies = top_results[0], top_results[1]
+
+    return scores[0].detach().item(), indicies[0].detach().item()
+
+
 ############################################################
 # Callback function called on each execution pass
 ############################################################
 def execute(request: SimpleText, ray: OpenfabricExecutionRay) -> SimpleText:
     output = []
     for text in request.text:
-        # TODO Add code here
-        response = ''
-        output.append(response)
+        score, indicie = find_match(text)
+        if score < 0.5:
+            answer = '''Sorry I am dumb, please try to ask me question about science, like:''' + \
+            f''' {choice(possible_questions)}'''
+        elif score < 0.7:
+            answer = f'I am not sure the answer might be:  {question_answer_mapper[questions[indicie]]}'
+        else:
+            answer = f'The answer is: {question_answer_mapper[questions[indicie]]}'
+
+        output.append(answer)
 
     return SimpleText(dict(text=output))
